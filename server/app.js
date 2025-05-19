d27feaf9d1dc4dd6b9b1c7d18f52ecb18b9cf8c8
meayushsriv <me.facteverse@gmail.com>
2025-05-19 05:42:29 +0530

    fixed app.js

--- server/app.js
+++ server/app.js
@@ -2,28 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { connectToQueue } = require("./config/queue"); // Only import connectToQueue
+const { connectToQueue } = require("./config/queue");
 const config = require("./config");
 const logger = require("./utils/logger");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Database connection
 mongoose
   .connect(config.dbUri)
   .then(() => logger.info("Connected to MongoDB"))
   .catch((error) => logger.error("MongoDB connection error:", error));
 
-// Queue connection - Simplified
 connectToQueue()
   .then(() => logger.info("RabbitMQ connected and consumer started"))
   .catch((error) => logger.error("Queue setup error:", error));
 
-// Routes
 const notificationRoutes = require("./routes/notification.routes");
 app.use("/notifications", notificationRoutes);
 app.use("/users", notificationRoutes);
